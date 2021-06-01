7b8143fae2204b8a560fc6bb0f96509b529a95c1
Abdullah AL-Noman <83629146+AL-Noman21@users.noreply.github.com>
2021-06-01 16:17:19 +0600

    Update backups.py

--- LEGEND/modules/backups.py
+++ LEGEND/modules/backups.py
@@ -343,7 +343,7 @@ def export_data(update, context):
     context.bot.sendDocument(
         current_chat_id,
         document=open("LEGENDBOT{}.backup".format(chat_id), "rb"),
-        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Thunder Bot` was specially made for notes.".format(
+        caption="*Successfully Exported backup:*\nChat:.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
